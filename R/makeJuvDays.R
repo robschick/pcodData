e39f2a661bd5d5150a86c1442caa1bd9d7870dad
= <=>
2014-09-26 15:22:59 +0100

    help file for juveniles

---
+++ R/makeJuvDays.R
@@ -0,0 +1,11 @@
+#' Making the parameter data frame for juveniles
+#'
+#'
+#' @source Interim PCOD Report
+#' @format Data frames with columns
+#' \describe{
+#' \item{hsjuvedays}{3 column data frame containing parameters governing the survival curve for juveniles: 1st is effect on survival, 2nd is days tolerated, 3rd is days before max effect}
+#' }
+#' @examples
+#'   hsjuvedays
+"hsjuvedays"
