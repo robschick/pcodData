dbb0ca081421a67ed5750805e90fbeca85f484f1
= <=>
2014-09-26 14:59:12 +0100

    Roxygen help file - check in

---
+++ R/makeElicitData.R
@@ -0,0 +1,11 @@
+#' Getting the elicitation data prepared into an R package
+#'
+#'
+#' @source Interim PCOD Report
+#' @format Data frames with columns
+#' \describe{
+#' \item{hsmaturedays}{3 Parameters governing the survival curve}
+#' }
+#' @examples
+#'   hsmaturedays
+"hsmaturedays"
