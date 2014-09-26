006202e8321ae1278b52b167c5345a20342d50e9
= <=>
2014-09-26 15:21:51 +0100

    exanding the help file for the.

--- R/makeElicitData.R
+++ R/makeElicitData.R
@@ -4,7 +4,7 @@
 #' @source Interim PCOD Report
 #' @format Data frames with columns
 #' \describe{
-#' \item{hsmaturedays}{3 Parameters governing the survival curve}
+#' \item{hsmaturedays}{3 column data frame containing parameters governing the survival curve: 1st is effect on survival, 2nd is days tolerated, 3rd is days before max effect}
 #' }
 #' @examples
 #'   hsmaturedays
